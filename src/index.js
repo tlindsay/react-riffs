bee93d34660c5ffd1e4f6b13f8829f7dca3bf8f4
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-03-27 14:45:24 -0500

    Add basic webmidi wrapper implementation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import WebMidi from 'webmidi';
+import retry from 'async-retry';
 
-export default class extends Component {
+class MidiContext extends Component {
+  constructor(props) {
+    super(...arguments);
+    this.state = { inputs: [] };
+
+    this.initializeMidi();
+  }
+
+  componentWillUnmount() {
+    console.info('Removing MIDI event listeners')
+    this.state.inputs.forEach((input) => input.removeListener());
+  }
+
+  async initializeMidi() {
+    await retry(async () => {
+      console.info('Attempting to initialize MIDI');
+      WebMidi.enable((err) => {
+        if (!err) {
+          console.info('MIDI enabled');
+          this.state.inputs = WebMidi.inputs;
+
+          if (this.props.debug) {
+            this.addDebugListeners();
+          }
+        }
+      });
+    });
+  }
+
+  addDebugListeners() {
+    this.state.inputs.forEach((input) => {
+      input.addListener('noteon', 'all', () => console.log('noteon'));
+      input.addListener('pitchbend', 'all', () => console.log('pitchbend'));
+      input.addListener('start', 'all', () => console.log('start'));
+      input.addListener('stop', 'all', () => console.log('stop'));
+      input.addListener('reset', 'all', () => console.log('reset'));
+    });
+  }
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <>{this.props.children}</>
+    );
   }
 }
+
+export default MidiContext;
