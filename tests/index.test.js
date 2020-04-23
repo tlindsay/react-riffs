2ed63744762fbb40e8865b36e9822534e25ad77a
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-04-23 15:34:56 -0500

    Switch tests to jest

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,23 +1,11 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import * as Riffs from '../src/';
 
-import Component from 'src/'
-
-describe('Component', () => {
-  let node
-
-  beforeEach(() => {
-    node = document.createElement('div')
-  })
-
-  afterEach(() => {
-    unmountComponentAtNode(node)
-  })
-
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
-    })
-  })
-})
+describe('the package', () => {
+  test('exports all of the correct hooks', () => {
+    expect(Riffs).toHaveProperty('useMidiInputs');
+    expect(Riffs).toHaveProperty('useNote');
+    expect(Riffs).toHaveProperty('useNotes');
+    expect(Riffs).toHaveProperty('useClock');
+    expect(Riffs).toHaveProperty('usePitchbend');
+  });
+});
