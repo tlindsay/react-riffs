d7eddfa08949eebe152f9bb7d5488c98350ecb3a
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-05-01 16:15:40 -0500

    Update demo to new useClock API

--- demo/src/index.js
+++ demo/src/index.js
@@ -12,14 +12,17 @@ function Demo() {
   let [midiInput] = useMidiInputs({ debug: true });
   let pressedKeys = useNotes(midiInput, 15);
   let bend = usePitchbend(midiInput);
-  let clock = useClock(midiInput);
+  let [clock] = useClock(midiInput);
 
   return (
     <div style={{fontFamily: 'monospace'}}>
       <div>Clock: {clock}</div>
       <div>Bend: {bend}</div>
       <ul>
-        {pressedKeys.map(({ note: n, velocity: v }, i) => <li key={i}>{n.name} {n.number} {v}</li>)}
+        {
+          pressedKeys.map(({ note: n, velocity: v }, i) =>
+            <li key={i}>{n.name} {n.number} {v}</li>)
+        }
       </ul>
     </div>
   );
